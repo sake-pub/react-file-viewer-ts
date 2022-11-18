a6240321d091752cd6dc45010c9469005cf448ee
Ryan Richwine <ryan@redtailtechnology.com>
2022-11-18 13:03:29 -0800

    initial commit

---
+++ src/components/_app.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+
+type Props = {
+  value?: number
+}
+
+export default function MyCounter({ value = 0 }: Props) {
+  const [counter, setCounter] = useState(value)
+
+  const onMinus = () => {
+    setCounter((prev) => prev - 1)
+  }
+
+  const onPlus = () => {
+    setCounter((prev) => prev + 1)
+  }
+
+  return (
+    <div>
+      <h1>Counter: {counter}</h1>
+      <button onClick={onMinus}>-</button>
+      <button onClick={onPlus}>+</button>
+    </div>
+  )
+}
